43c6525b77f33880ea9ed9a78bdd0923cfb9aeb1
Kaushal Patel <kaushal.patel@metrosystems.net>
2018-09-27 11:36:00 +0530

    angular 6 first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestService } from './services/test-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular6';
+  message = null
+  constructor (private service: TestService) {
+
+  }
+
+  ngOnInit () {
+    console.log('ng on init')
+    this.service.fetch('http://demo1230338.mockable.io/eac')
+      .subscribe((response: any) => {
+        console.log('test servic e response : ', response)
+        this.message = response.msg
+      })
+  }
 }
